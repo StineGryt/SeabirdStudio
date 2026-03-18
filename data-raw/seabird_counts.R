c2086c369a174d99bdeab6a92719625039c9f319
StineGryt <stgry5517@uib.no>
2026-03-18 15:24:21 +0100

    Initial commit. add data processing pipeline

---
+++ data-raw/seabird_counts.R
@@ -0,0 +1,42 @@
+## code to prepare `seabird_counts`
+
+
+library(readxl)
+library(dplyr)
+
+# Load the JNCC dataset
+raw <- read_excel("data-raw/comparative-seabirds-count-dataset-revised-20240708.xlsx",
+                  sheet = "1_SC_S2K_dataset",
+                  skip = 3)
+
+
+# Clean and simplify dataset
+seabird_counts <- raw |>
+  select(
+    colony = Site,
+    species = Species,
+    count = `Seabirds Count adjusted count`,
+  ) |>
+  mutate(count = as.numeric(count)) |>
+  filter(!is.na(count), count > 0)
+
+# ---SUBSET---
+# Keep top 10 species
+top_species <- seabird_counts |>
+  count(species, sort = TRUE) |>
+  slice_head(n = 10) |>
+  pull(species)
+
+# Keep top 25 colonies
+top_colonies <- seabird_counts |>
+  count(colony, sort = TRUE) |>
+  slice_head(n = 25) |>
+  pull(colony)
+
+seabird_counts <- seabird_counts |>
+  filter(
+    species %in% top_species,
+    colony %in% top_colonies
+  )
+
+usethis::use_data(seabird_counts, overwrite = TRUE)
